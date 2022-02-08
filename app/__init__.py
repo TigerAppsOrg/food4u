fa927dfe9c0fb3953c6fb8435da2b94f81e6d3e5
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-08 15:12:37 -0500

    pics submission fix

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-import eventlet
 
 # from flask_mail import Mail
-
+import eventlet
 eventlet.monkey_patch()
+from requests.packages.urllib3.util.ssl_ import create_urllib3_context
+create_urllib3_context()
+
 app = Flask(__name__)
 CORS(app)
 # mail = Mail(app)
