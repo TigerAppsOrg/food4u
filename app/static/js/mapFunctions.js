4280cfea97017328ec56228120ea6af2764feb1f
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-10 18:44:10 -0500

    change zoom for main map

--- app/static/js/mapFunctions.js
+++ app/static/js/mapFunctions.js
@@ -123,7 +123,7 @@ function initEditFormMap() {
 
 function initMap() {
     main_map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15.7,
+        zoom: 16,
         center: princetonCoords,
         clickableIcons: false,
         streetViewControl: false,
