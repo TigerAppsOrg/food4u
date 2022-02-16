d9c4d78996f881a9a9a5956d9a4dd923dfdce3e9
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-16 05:01:06 -0500

    test scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -11,18 +11,19 @@ from flask import copy_current_request_context
 
 
 def get_update():
-    time = datetime.datetime.utcnow()
-    events = Event.query.all()
-    db.session.commit()
-    for event in events:
-        if event.end_time is None or time > (event.end_time + datetime.timedelta(hours=1)):
-            delete_data(event)
-            events_dict = fetch_events()
-            socket_io.emit('update', events_dict, broadcast=True)
-            active_event_count = fetch_active_events_count()
-            print(events_dict)
-            print(active_event_count)
-            socket_io.emit('active_event_count', active_event_count, broadcast=True)
+    with app.app_context():
+        time = datetime.datetime.utcnow()
+        events = Event.query.all()
+        db.session.commit()
+        for event in events:
+            if event.end_time is None or time > (event.end_time + datetime.timedelta(hours=1)):
+                delete_data(event)
+                events_dict = fetch_events()
+                socket_io.emit('update', events_dict, broadcast=True)
+                active_event_count = fetch_active_events_count()
+                print(events_dict)
+                print(active_event_count)
+                socket_io.emit('active_event_count', active_event_count, broadcast=True)
 
 
 scheduler_trash_markers.add_job("job_update", get_update, trigger="interval", seconds=20)
