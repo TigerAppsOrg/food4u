ff598b88e718592b9aec9ee908f378e405000f07
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-16 00:36:42 -0500

    bug fix in fetching # of markers on map

--- app/scheduler.py
+++ app/scheduler.py
@@ -4,7 +4,7 @@ import datetime
 from app import db
 from app.models import Event
 from app.routes import socket_io
-from app.helpers import fetch_events
+from app.helpers import fetch_events, fetch_active_events_count
 
 
 def update_data():
@@ -16,6 +16,8 @@ def update_data():
             delete_data(event)
             events_dict = fetch_events()
             socket_io.emit('update', events_dict, broadcast=True)
+            active_event_count = fetch_active_events_count()
+            socket_io.emit('active_event_count', active_event_count, broadcast=True)
 
 
 scheduler_trash_markers.add_job("job_update", update_data, trigger="interval", seconds=20)
