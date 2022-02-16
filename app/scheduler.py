ffc4c11a942f5ad900fddbf395143cf19a2b35b9
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-16 05:38:59 -0500

    test scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -2,12 +2,9 @@ from extensions import scheduler_trash_markers
 from app.helpers import delete_data
 import datetime
 from app import db
-from __init__ import app
+from app import app
 from app.models import Event
 from app.routes import socket_io
-from app.helpers import fetch_events, fetch_active_events_count
-import threading
-from flask import copy_current_request_context
 
 
 def get_update():
@@ -18,9 +15,8 @@ def get_update():
         for event in events:
             if event.end_time is None or time > (event.end_time + datetime.timedelta(hours=1)):
                 delete_data(event)
-                events_dict = fetch_events()
-                socket_io.emit('update', events_dict, broadcast=True)
-                active_event_count = fetch_active_events_count()
+                socket_io.emit('client_update', broadcast=True)
+                active_event_count = Event.query.count()
                 socket_io.emit('active_event_count', active_event_count, broadcast=True)
 
 
