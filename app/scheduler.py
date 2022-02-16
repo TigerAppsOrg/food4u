a24113f1a8578696a481facf9dffc7f4e6f370c5
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-15 22:30:59 -0500

    functions not needed

--- app/scheduler.py
+++ app/scheduler.py
@@ -3,9 +3,6 @@ from app.helpers import delete_data
 import datetime
 from app import db
 from app.models import Event
-from app.helpers import fetch_active_events_count
-
-from app.routes import socket_io
 
 
 def update_data():
@@ -15,8 +12,6 @@ def update_data():
     for event in events:
         if event.end_time is None or time > (event.end_time + datetime.timedelta(hours=1)):
             delete_data(event)
-            active_event_count = fetch_active_events_count()
-            socket_io.emit('active_event_count', active_event_count, broadcast=True)
 
 
 scheduler_trash_markers.add_job("job_update", update_data, trigger="interval", seconds=20)
