c1a4a7b81eef9f52bbd523037adf408b7caf13f3
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-02-15 23:53:48 -0500

    markers disappearing when edited bug fix

--- app/routes.py
+++ app/routes.py
@@ -834,3 +834,9 @@ def logout():
     cas_client = CasClient()
     cas_client.authenticate()
     cas_client.logout('index')
+
+
+@socket_io.on("update")
+def fetch_events_emit():
+    events = fetch_events()
+    socket_io.emit("update", events, broadcast=False)
--- app/static/js/globalVariables.js
+++ app/static/js/globalVariables.js
@@ -1,3 +1,4 @@
+let socket = io();
 let main_map;
 const princetonCoords = {lat: 40.346916, lng: -74.655304};
 let formMarker;
