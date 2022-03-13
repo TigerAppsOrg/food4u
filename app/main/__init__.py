80f919313da8fee6cd07d1a64ee744b4c08e68dc
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-03-12 19:18:59 -0500

    fix update markers bug

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates', static_url_path='/main')
 
-from . import routes, helpers
+from . import routes, helpers, socket_events
--- app/main/socket_events.py
+++ app/main/socket_events.py
@@ -1,9 +1,8 @@
 from .. import socket_io
-from flask_socketio import emit
 from app.main.helpers import fetch_events
 
 
 @socket_io.on("update")
 def fetch_events_emit():
     events = fetch_events()
-    emit("update", events, broadcast=False)
+    socket_io.emit("update", events, broadcast=False)
