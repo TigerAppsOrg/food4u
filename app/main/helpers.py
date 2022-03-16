4b9cbdad2bb4dd5961387c05b74b62d99d2b2525
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-03-15 22:10:36 -0400

    fix send comment email style

--- app/main/helpers.py
+++ app/main/helpers.py
@@ -66,10 +66,9 @@ def send_comment_email_to_op(event, comment, commenter):
     email_html_suffix += '</div>'
 
     email_html = '<p style="color:#f58025;"><strong>' \
-                 'Your free food event has been received a comment:<\strong></p>' \
-                 + comment + \
-                 '<br>' + \
-                 "<p><strong>Commented by: <strong>" + str(commenter) + "<strong></p>"
+                 'Your free food event has been received a comment:<\strong></p>' + \
+                 comment + \
+                 '<br> <p><strong>Commented by: <strong>" + str(commenter)' + "<strong></p>"
     email_html += '<p style="color:#f58025;"><strong> ' \
                   f"<a href='https://food4u.tigerapps.org/index/{event.id}'" \
                   f"target='_blank' rel='noopener noreferrer'>Click here" \
