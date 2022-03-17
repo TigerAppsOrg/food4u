894b75694b3ed9148623ae7577e15bd479030fa6
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-03-17 09:40:40 -0400

    change comments_modal_table.html style

--- app/main/helpers.py
+++ app/main/helpers.py
@@ -68,7 +68,7 @@ def send_comment_email_to_op(event, comment, commenter):
     email_html = '<p style="color:#f58025;"><strong>' \
                  'Your free food event has been received a comment:</strong></p>' + \
                  comment + \
-                 '<br> <p><strong>Commented by: <strong>" + str(commenter)' + "<strong></p>"
+                 '<br> <p><strong>Commented by: <strong>' + str(commenter) + "<strong></p>"
     email_html += '<p style="color:#f58025;"><strong> ' \
                   f"<a href='https://food4u.tigerapps.org/index/{event.id}'" \
                   f"target='_blank' rel='noopener noreferrer'>Click here" \
